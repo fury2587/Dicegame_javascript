38bd389cb2d9df03065cde75740871c1e30af2a1
fury2587 <145056735+fury2587@users.noreply.github.com>
2023-09-30 23:32:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomsource = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" +randomsource + ".png"
+
+var randomImageSource = "images/" +randomDiceImage;
+
+document.querySelectorAll("img")[0].setAttribute('src',randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomsource>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+  
